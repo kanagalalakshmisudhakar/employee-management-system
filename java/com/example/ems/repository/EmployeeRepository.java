1bc882d092bd6d19e3b243dad9001df930d97a91
Sudhakar <ammalakshmisudhakar@gmail.com>
2026-01-08 14:35:12 +0530

    Remove duplicate root backend files

--- java/com/example/ems/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ems.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
