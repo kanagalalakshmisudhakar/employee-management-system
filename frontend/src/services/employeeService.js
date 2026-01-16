c18e2efeed9a06fef2690bb7fbc31843de7424e3
Sudhakar <ammalakshmisudhakar@gmail.com>
2026-01-17 00:17:06 +0530

    added the render server URL

--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/employees";
+const API_URL = "https://ems-deployment-latest-g8y6.onrender.com/api/employees";
 
 export const getAllEmployees = () => {
   return axios.get(API_URL);
